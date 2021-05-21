241b9194bb6285464695c61ab59a889552716901
Keyong Han <keyonghan@google.com>
2021-05-21 22:04:15 +0000

    Add infra step for firebaselab recipe
    Bug: https://github.com/flutter/flutter/issues/78632, https://github.com/flutter/flutter/issues/82866
    Change-Id: I53d2c5247f45ea963ebb5f162f6492ab939ec292
    Reviewed-on: https://flutter-review.googlesource.com/c/recipes/+/13760
    Commit-Queue: Keyong Han <keyonghan@google.com>
    Reviewed-by: Jenn Magder <magder@google.com>
    Reviewed-by: Godofredo Contreras <godofredoc@google.com>

--- recipes/firebaselab/firebaselab.py
+++ recipes/firebaselab/firebaselab.py
@@ -41,74 +41,92 @@ def RunSteps(api):
   physical_devices = [
       # Physical devices - use only highly available devices to avoid timeouts.
       # Pixel 3
-      '--device', 'model=blueline,version=28',
+      '--device',
+      'model=blueline,version=28',
       # Pixel 4
-      '--device', 'model=flame,version=29',
+      '--device',
+      'model=flame,version=29',
       # Moto Z XT1650
-      '--device', 'model=griffin,version=24',
+      '--device',
+      'model=griffin,version=24',
   ]
 
   virtual_devices = [
       # Virtual devices for API level coverage.
-      '--device', 'model=Nexus5,version=19',
+      '--device',
+      'model=Nexus5,version=19',
       # SDK 20 not available virtually or physically.
-      '--device', 'model=Nexus5,version=21',
-      '--device', 'model=Nexus5,version=22',
-      '--device', 'model=Nexus5,version=23',
+      '--device',
+      'model=Nexus5,version=21',
+      '--device',
+      'model=Nexus5,version=22',
+      '--device',
+      'model=Nexus5,version=23',
       # SDK 24 is run on a physical griffin/Moto Z above.
-      '--device', 'model=Nexus6P,version=25',
-      '--device', 'model=Nexus6P,version=26',
-      '--device', 'model=Nexus6P,version=27',
+      '--device',
+      'model=Nexus6P,version=25',
+      '--device',
+      'model=Nexus6P,version=26',
+      '--device',
+      'model=Nexus6P,version=27',
       # SDK 28 is run on a physical blueline/Pixel 3 above.
       # SDK 29 is run on a physical flame/Pixel 4 above.
-      '--device', 'model=NexusLowRes,version=30',
+      '--device',
+      'model=NexusLowRes,version=30',
   ]
 
   test_configurations = (
       (
-          'Build appbundle',
-          ['flutter', 'build', 'appbundle', '--target-platform',
-                'android-arm,android-arm64'],
-          'build/app/outputs/bundle/release/app-release.aab',
+          'Build appbundle', [
+              'flutter', 'build', 'appbundle', '--target-platform',
+              'android-arm,android-arm64'
+          ], 'build/app/outputs/bundle/release/app-release.aab',
           physical_devices
       ),
       (
-          'Build apk',
-          ['flutter', 'build', 'apk', '--debug', '--target-platform',
-                'android-x86'],
-          'build/app/outputs/flutter-apk/app-debug.apk',
-          virtual_devices
+          'Build apk', [
+              'flutter', 'build', 'apk', '--debug', '--target-platform',
+              'android-x86'
+          ], 'build/app/outputs/flutter-apk/app-debug.apk', virtual_devices
       ),
   )
 
   with api.context(env=env, env_prefixes=env_prefixes, cwd=checkout_path):
     api.step('flutter doctor', ['flutter', 'doctor', '-v'])
-    api.step('download dependencies', ['flutter', 'update-packages'])
+    api.step(
+        'download dependencies', ['flutter', 'update-packages'],
+        infra_step=True,
+    )
 
   test_path = checkout_path.join('dev', 'integration_tests', task_name)
   with api.step.nest('test_execution') as presentation:
     with api.context(env=env, env_prefixes=env_prefixes, cwd=test_path):
       task_id = api.swarming.task_id
-      api.gcloud('--quiet', 'config', 'set', 'project', 'flutter-infra')
+      api.gcloud(
+          '--quiet',
+          'config',
+          'set',
+          'project',
+          'flutter-infra',
+          infra_step=True,
+      )
       for step_name, build_command, binary, devices in test_configurations:
         api.step(step_name, build_command)
         firebase_cmd = [
-            'firebase', 'test', 'android', 'run', '--type', 'robo',
-            '--app', binary,
-            '--timeout', '2m',
+            'firebase', 'test', 'android', 'run', '--type', 'robo', '--app',
+            binary, '--timeout', '2m',
             '--results-bucket=gs://%s' % gcs_bucket,
             '--results-dir=%s/%s' % (task_name, task_id)
         ] + devices
+
         # See https://firebase.google.com/docs/test-lab/android/command-line#script_exit_codes
         # If the firebase command fails with 1, it's likely an HTTP issue that
         # will resolve on a retry. If it fails on 15 or 20, it's explicitly
         # an infra failure on the FTL side, so we should just retry.
         def run_firebase():
           return api.gcloud(*firebase_cmd)
-        api.retry.wrap(
-          run_firebase,
-          max_attempts=3,
-          retriable_ret=(1, 15, 20))
+
+        api.retry.wrap(run_firebase, max_attempts=3, retriable_ret=(1, 15, 20))
 
       logcat_path = '%s/%s/*/logcat' % (task_name, task_id)
       tmp_logcat = api.path['cleanup'].join('logcat')
@@ -124,14 +142,12 @@ def GenTests(api):
       api.properties(task_name='the_task')
   )
 
-  yield api.test(
-      'failure 15', api.repo_util.flutter_environment_data()
-  ) + api.step_data(
-     'test_execution.gcloud firebase', retcode=15
-  )
+  yield api.test('failure 15',
+                 api.repo_util.flutter_environment_data()) + api.step_data(
+                     'test_execution.gcloud firebase', retcode=15
+                 )
 
-  yield api.test(
-      'failure 10', api.repo_util.flutter_environment_data()
-  ) + api.step_data(
-     'test_execution.gcloud firebase', retcode=10
-  )
+  yield api.test('failure 10',
+                 api.repo_util.flutter_environment_data()) + api.step_data(
+                     'test_execution.gcloud firebase', retcode=10
+                 )

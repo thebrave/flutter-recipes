98f439689b1afe86eb759268b9bee0d3874e0e8b
Christopher Fujino <fujino@google.com>
2021-06-11 00:23:54 +0000

    revert engine_2_2_0.py recipe changes from https://flutter-review.googlesource.com/c/recipes/+/13960
    Change-Id: I5fc29c4b394794c7553fb6dde3915563fca6c700
    Reviewed-on: https://flutter-review.googlesource.com/c/recipes/+/14581
    Reviewed-by: Jason Simmons <jsimmons@google.com>
    Commit-Queue: Christopher Fujino <fujino@google.com>

--- recipes/engine_2_2_0.py
+++ recipes/engine_2_2_0.py
@@ -164,7 +164,7 @@ def GetFuchsiaOutputFiles(product):
   ]
 
 
-def GetFuchsiaOutputDirs(product):
+def GetFuchsiaOutputDirs(product, build_mode, target_arch):
   return [
       'dart_jit_%srunner_far' % ('product_' if product else ''),
       'dart_aot_%srunner_far' % ('product_' if product else ''),
@@ -173,7 +173,7 @@ def GetFuchsiaOutputDirs(product):
       'dart_runner_patched_sdk',
       'flutter_runner_patched_sdk',
       'clang_x64',
-      '.build-id',
+      'flutter-debug-symbols-%s-fuchsia-%s' % (build_mode, target_arch),
   ]
 
 
@@ -832,7 +832,8 @@ def UploadFuchsiaDebugSymbols(api):
     symbol_dirs = []
     for mode in modes:
       base_dir = 'fuchsia_%s_%s' % (mode, arch)
-      symbol_dir = checkout.join('out', base_dir, '.build-id')
+      symbols_basename = 'flutter-debug-symbols-%s-fuchsia-%s' % (mode, arch)
+      symbol_dir = checkout.join('out', base_dir, symbols_basename)
       symbol_dirs.append(symbol_dir)
     UploadFuchsiaDebugSymbolsToSymbolServer(api, arch, symbol_dirs)
     with api.os_utils.make_temp_directory('FuchsiaDebugSymbols') as temp_dir:
@@ -871,7 +872,7 @@ def BuildFuchsia(api):
   for arch, build_mode in fuchsia_build_pairs:
     gn_args = ['--fuchsia', '--fuchsia-cpu', arch, '--runtime-mode', build_mode]
     product = build_mode == 'release'
-    fuchsia_output_dirs = GetFuchsiaOutputDirs(product)
+    fuchsia_output_dirs = GetFuchsiaOutputDirs(product, build_mode, arch)
     props = {
         'builds': [{
             'gn_args': gn_args,

06c55d9a14a7d7e5cd70a69315a2bccf6c6fa83d
Godofredo Contreras <godofredoc@google.com>
2022-01-20 17:04:36 +0000

    Fix codesign build validation.
    This build was failing because "git checkout" requires a "git fetch"
    first.
    
    Bug: https://github.com/flutter/flutter/issues/96889
    Change-Id: I9f24b66248f1cc1781f7cabcbc69d07cce6de80a
    Reviewed-on: https://flutter-review.googlesource.com/c/recipes/+/25501
    Reviewed-by: Christopher Fujino <fujino@google.com>
    Commit-Queue: Godofredo Contreras <godofredoc@google.com>

--- recipe_modules/adhoc_validation/resources/verify_binaries_codesigned.sh
+++ recipe_modules/adhoc_validation/resources/verify_binaries_codesigned.sh
@@ -4,4 +4,10 @@
 
 set -e
 
+# Checking out master here is ok even though this test runs on release branches
+# because the checkout is used only to download the conductor tool but the validation
+# is run on the binaries for the commit passed as property to the builder.
+git fetch origin master:master
+
+# Run the actual validation.
 ./dev/conductor/bin/conductor codesign --verify

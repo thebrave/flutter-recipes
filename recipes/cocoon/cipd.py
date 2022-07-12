7a702db7c1c8dc057d95e0d23849c885b3463ff3
Xilai Zhang <xilaizhang@google.com>
2022-07-12 17:21:25 +0000

    recipe to run for codesign
    tested through `led get-builder 'luci.flutter.prod:Mac device_doctor' > /tmp/test.json`, and `cat /tmp/test.json |  led edit-recipe-bundle | led launch`. test link is https://ci.chromium.org/raw/build/logs.chromium.org/flutter/led/xilaizhang_google.com/ca661824e4debd72dcfab48ba84dd1bf1ab2bab8345d345418f227ba70ca9bae/+/build.proto?server=chromium-swarm.appspot.com.
    
    the cipd can be viewed at https://chrome-infra-packages.appspot.com/p/flutter/codesign.
    
    Change-Id: I5352ec19c4bce8ec7c0db737bf4af871f9f3542e
    Reviewed-on: https://flutter-review.googlesource.com/c/recipes/+/31240
    Reviewed-by: Keyong Han <keyonghan@google.com>
    Commit-Queue: Xilai Zhang <xilaizhang@google.com>
    Reviewed-by: Casey Hillers <chillers@google.com>

---
+++ recipes/cocoon/cipd.py
@@ -0,0 +1,81 @@
+# Copyright 2020 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from platform import platform
+from recipe_engine.recipe_api import Property
+
+PYTHON_VERSION_COMPATIBILITY = 'PY3'
+
+DEPS = [
+    'flutter/repo_util',
+    'flutter/yaml',
+    'recipe_engine/cipd',
+    'recipe_engine/context',
+    'recipe_engine/json',
+    'recipe_engine/path',
+    'recipe_engine/platform',
+    'recipe_engine/properties',
+    'recipe_engine/step',
+]
+
+
+# This recipe builds the codesign CIPD package.
+def RunSteps(api):
+  start_path = api.path['start_dir']
+  cocoon_dir = start_path.join('cocoon')
+  cocoon_git_rev = api.repo_util.checkout(
+      'cocoon',
+      cocoon_dir,
+      ref='refs/heads/main',
+  )
+
+  # Get properties from ci.yaml.
+  # Assume ci.yaml has the following properties
+  #   - name: Mac codesign
+  #     bringup: true
+  #     postsubmit: false
+  #     recipe: cocoon/codesign
+  #     properties:
+  #       add_recipes_cq: "true"
+  #     script: device_doctor/tool/build.sh
+  #     cipd_name: flutter/device_doctor/mac-amd64
+
+  script_path_list = api.properties.get('script')
+  cipd_full_name = api.properties.get('cipd_name')
+  project_name = cipd_full_name.split('/')[1]
+  project_path = cocoon_dir.join(project_name)
+
+  build_file = cocoon_dir.join(script_path_list)
+
+  cmd = [build_file]
+  if not api.platform.is_win:
+    cmd = ['bash', build_file]
+
+  with api.context(cwd=project_path):
+    api.step('build package', cmd)
+
+    cipd_zip_path = project_name + '.zip'
+
+    api.cipd.build(project_path.join('build'), cipd_zip_path, cipd_full_name)
+    api.cipd.register(cipd_full_name, cipd_zip_path)
+
+
+def GenTests(api):
+  yield api.test(
+      'cipd_mac',
+      api.properties(
+          script='codesign/tool/build.sh',
+          cipd_name='flutter/codesign/mac-amd64'
+      ),
+      api.platform('mac', 64),
+  )
+
+  yield api.test(
+      'cipd_win',
+      api.properties(
+          script='device_doctor\\tool\\build.bat',
+          cipd_name='flutter/device_doctor/windows-amd64'
+      ),
+      api.platform('win', 64),
+  )

0499182eee1e59bae9a67fd6cc1d13aca6c8a38e
Godofredo Contreras <godofredoc@google.com>
2022-09-20 18:25:14 +0000

    Reland: "Start uploading docs to GCS."
    The previous cl was reverted because both "docs test" and "docs deploy"
    were uploading the zip artifact and the builders were not allowing to
    override the destination artifacts.
    
    With this updated version only "docs deploy" will be uploading the
    artifacts and a second change is enabling override on retry for
    framework builders.
    
    Bug: flutter/flutter#111921
    Change-Id: Ifcbadfe912922544d7ce4dce50bf9392cd5c1088
    Reviewed-on: https://flutter-review.googlesource.com/c/recipes/+/34101
    Commit-Queue: Godofredo Contreras <godofredoc@google.com>
    Reviewed-by: Casey Hillers <chillers@google.com>

--- recipe_modules/adhoc_validation/api.py
+++ recipe_modules/adhoc_validation/api.py
@@ -59,9 +59,9 @@ class AddhocValidationApi(recipe_api.RecipeApi):
             self.m.test_utils.run_test(validation, [resource_name])
       else:
         with self.m.context(env=env, env_prefixes=env_prefixes):
-          self.m.test_utils.run_test(validation, [resource_name])
+          docs_path = checkout_path.join('dev', 'docs')
           if validation == 'docs' and self.m.properties.get('firebase_project'):
-            docs_path = checkout_path.join('dev', 'docs')
+            self.m.bucket_util.upload_folder('Upload API Docs', docs_path, 'doc', "api_docs.zip")
             project = self.m.properties.get('firebase_project')
             self.m.firebase.deploy_docs(
                 env=env,

73f0f1f98b70bd237b7a1384922399a17d4c30af
Godofredo Contreras <godofredoc@google.com>
2022-10-27 22:53:26 +0000

    Module to calculate archive locations and upload to GCS.
    This module has methods to flatten out the archives generated by builds
    to ensure a single file is passed to the provenance generation at the
    time.
    
    Bug: https://github.com/flutter/flutter/issues/113193
    Change-Id: Ic3c6319a6d2663e3c487ccae69aaa9d04acb1cf0
    Reviewed-on: https://flutter-review.googlesource.com/c/recipes/+/35162
    Reviewed-by: Yusuf Mohsinally <mohsinally@google.com>
    Commit-Queue: Godofredo Contreras <godofredoc@google.com>

---
+++ recipe_modules/archives/examples/full.py
@@ -0,0 +1,54 @@
+# Copyright 2022 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from recipe_engine.post_process import DoesNotRun, Filter, StatusFailure
+
+DEPS = [
+    'flutter/archives',
+    'recipe_engine/buildbucket',
+    'recipe_engine/path',
+    'recipe_engine/raw_io',
+]
+
+
+def RunSteps(api):
+  checkout = api.path['start_dir']
+  config = {
+      "name": "android_profile",
+      "type": "gcs",
+      "base_path": "out/android_profile/zip_archives/",
+      "include_paths": [
+          "out/android_profile/zip_archives/android-arm-profile/artifacts.zip",
+          "out/android_profile/zip_archives/android-arm-profile/linux-x64.zip",
+          "out/android_profile/zip_archives/android-arm-profile/symbols.zip",
+          "out/android_profile/zip_archives/download.flutter.io"
+      ]
+  }
+  results = api.archives.engine_v2_gcs_paths(checkout, config)
+  api.archives.upload_artifact(results[0].local, results[0].remote)
+
+
+def GenTests(api):
+  yield api.test(
+      'basic',
+      api.step_data(
+          'git rev-parse',
+          stdout=api.raw_io
+          .output_text('12345abcde12345abcde12345abcde12345abcde\n')
+      )
+  )
+  yield api.test(
+      'monorepo_gcs',
+      api.buildbucket.ci_build(
+          project='dart',
+          bucket='ci.sandbox',
+          git_repo='https://dart.googlesource.com/monorepo',
+          git_ref='refs/heads/main'
+      ),
+      api.step_data(
+          'git rev-parse',
+          stdout=api.raw_io
+          .output_text('12345abcde12345abcde12345abcde12345abcde\n')
+      )
+  )

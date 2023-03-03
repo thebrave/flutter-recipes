3d708a2aeda0b7924479e6ec7b6793b276110228
Godofredo Contreras <godofredoc@google.com>
2023-03-03 23:04:20 +0000

    Move packaging_v2 to packaging for a soft transition.
    packaging/packaging recipes was replace with packaging_v2/packaging_v2.
    Now that packaging_v2 has been running on prod for a few months we are
    renaming it as packaging and remove packaging_v2.
    
    Bug: https://github.com/flutter/flutter/issues/121735
    Change-Id: I4857c673170e70baf14b154b080d86e17b1e2b9b
    Reviewed-on: https://flutter-review.googlesource.com/c/recipes/+/39940
    Reviewed-by: Yusuf Mohsinally <mohsinally@google.com>
    Commit-Queue: Godofredo Contreras <godofredoc@google.com>

--- recipes/packaging/packaging.py
+++ recipes/packaging/packaging.py
@@ -9,6 +9,7 @@ import re
 DEPS = [
     'depot_tools/depot_tools',
     'depot_tools/git',
+    'flutter/archives',
     'flutter/flutter_bcid',
     'flutter/flutter_deps',
     'flutter/repo_util',
@@ -23,7 +24,13 @@ DEPS = [
     'recipe_engine/step',
 ]
 
-PACKAGED_REF_RE = re.compile(r'^refs/heads/(beta|stable)$')
+PACKAGED_REF_RE = re.compile(r'^refs/heads/(.+)$')
+
+PLATFORMS_MAP = {
+    'win': 'windows',
+    'mac': 'macos',
+    'linux': 'linux'
+}
 
 
 @contextmanager
@@ -43,15 +50,21 @@ def Install7za(api):
 
 
 def CreateAndUploadFlutterPackage(api, git_hash, branch, packaging_script):
-  """Prepares, builds, and uploads an all-inclusive archive package."""
+  """Prepares, builds, and uploads an all-inclusive archive package.
+
+    Args:
+      git_hash(str): Hash corresponding to git commit.
+      branch(str): Name of the flutter branch.
+      packaging_script(str): Script that will prepare, create and publish a flutter archive.
+  """
   flutter_executable = 'flutter' if not api.platform.is_win else 'flutter.bat'
   dart_executable = 'dart' if not api.platform.is_win else 'dart.exe'
   work_dir = api.path['start_dir'].join('archive')
   api.step('flutter doctor', [flutter_executable, 'doctor'])
   api.step('download dependencies', [flutter_executable, 'update-packages', '-v'])
+  api.flutter_bcid.report_stage(BcidStage.COMPILE.value)
   api.file.rmtree('clean archive work directory', work_dir)
   api.file.ensure_directory('(re)create archive work directory', work_dir)
-  api.flutter_bcid.report_stage(BcidStage.COMPILE.value)
   with Install7za(api):
     with api.context(cwd=api.path['start_dir']):
       step_args = [
@@ -60,19 +73,69 @@ def CreateAndUploadFlutterPackage(api, git_hash, branch, packaging_script):
           '--revision=%s' % git_hash,
           '--branch=%s' % branch
       ]
-      if not api.runtime.is_experimental:
-        step_args.append('--publish')
-      api.step('prepare, create and publish a flutter archive', step_args)
+      api.step('prepare and create a flutter archive', step_args)
 
-      # TODO(drewroengoogle): Utilize this path later on when we publish to gcs manually
-      flutter_package_absolute_path = GetFlutterPackageAbsolutePath(api, work_dir)
+      flutter_pkg_absolute_path = GetFlutterPackageAbsolutePath(api, work_dir)
+      file_name = api.path.basename(flutter_pkg_absolute_path)
+      platform_name = PLATFORMS_MAP[api.platform.name]
+      dest_archive = '/%s/%s' % (branch, platform_name)
+      dest_gs = 'gs://flutter_infra_release/releases%s' % dest_archive
+      api.flutter_bcid.report_stage(BcidStage.UPLOAD.value)
+      if branch in ('beta', 'stable') and api.flutter_bcid.is_official_build():
+        # only publish using upload_artifact if is branch is beta or stable
+        # publish both package and metadata which has been updated
+        # by the packaging_script
+        pkg_gs_path = '%s/%s' % (dest_gs, file_name)
+        metadata_absolute_path = GetFlutterMetadataAbsolutePath(api, work_dir)
+        metadata_filename = api.path.basename(metadata_absolute_path)
+        metadata_dst = 'gs://flutter_infra_release/releases'
+        metadata_gs_path = "%s/%s" % (metadata_dst, metadata_filename)
+        # This metadata file is used by the website, so we don't want a long
+        # latency between publishing a release and it being available on the
+        # site.
+        headers = {'Cache-Control': 'max-age=60'}
+        api.archives.upload_artifact(metadata_absolute_path, metadata_gs_path, metadata=headers)
+      else:
+        # add experimental subpath if branch is not beta or stable
+        pkg_gs_path = '%s/%s/%s' % (dest_gs, 'experimental', file_name)
+      # Do not upload on presubmit.
+      if ((not api.runtime.is_experimental) and (api.flutter_bcid.is_official_build() or
+          api.flutter_bcid.is_prod_build())):
+        api.archives.upload_artifact(flutter_pkg_absolute_path, pkg_gs_path)
+        api.flutter_bcid.upload_provenance(flutter_pkg_absolute_path, pkg_gs_path)
+      api.flutter_bcid.report_stage(BcidStage.UPLOAD_COMPLETE.value)
 
 
 def GetFlutterPackageAbsolutePath(api, work_dir):
-  """Gets full flutter package directory if it exists."""
-  files = api.file.glob_paths('get flutter archive file name', work_dir, '*flutter*.tar.xz', test_data=['flutter-archive-package.tar.xz'])
+  """Gets full flutter package directory if it exists.
+
+  Args:
+    work_dir(str): Path to working directory.
+
+  Returns:
+    str: Fully qualified path to flutter package directory.
+  """
+  suffix = 'tar.xz' if api.platform.is_linux else 'zip'
+  files = api.file.glob_paths('get flutter archive file name', work_dir,
+                              '*flutter*.%s' % suffix, test_data=['flutter-archive-package.%s' % suffix])
   return files[0] if len(files) == 1 else None
 
+def GetFlutterMetadataAbsolutePath(api, work_dir):
+  """Gets local metadata absolute path.
+
+  Returns:
+    str: Path to the metadata file which contains a platform suffix.
+  """
+  metadata_filename = "releases_"
+  if api.platform.is_linux:
+    metadata_filename += "linux.json"
+  elif api.platform.is_mac:
+    metadata_filename += "macos.json"
+  elif api.platform.is_win:
+    metadata_filename += "windows.json"
+  files = api.file.glob_paths('get flutter archive file name', work_dir,
+                            metadata_filename, test_data=['releases_linux.json'])
+  return files[0] if len(files) == 1 else None
 
 def RunSteps(api):
   api.flutter_bcid.report_stage(BcidStage.START.value)
@@ -105,22 +168,21 @@ def RunSteps(api):
   api.flutter_deps.required_deps(
       env, env_prefixes, api.properties.get('dependencies', [])
   )
-  release_ref = api.properties.get('release_ref') or git_ref
 
   packaging_script = checkout_path.join(
       'dev', 'bots', 'prepare_package.dart'
   )
   with api.context(env=env, env_prefixes=env_prefixes):
     with api.depot_tools.on_path():
-      if release_ref:
-        match = PACKAGED_REF_RE.match(release_ref)
-        if match:
-          branch = match.group(1)
-          CreateAndUploadFlutterPackage(api, release_git_hash, branch, packaging_script)
-          # Nothing left to do on a packaging branch.
-          return
+      match = PACKAGED_REF_RE.match(git_ref)
+      if match:
+        branch = match.group(1)
+        CreateAndUploadFlutterPackage(api, release_git_hash, branch, packaging_script)
+        # Nothing left to do on a packaging branch.
+        return
       raise api.step.StepFailure(
-          'could not determine the release branch: either "release_ref" or "git_ref" properties should be set when manually triggering builds'
+          'could not determine the release branch: "git_ref"'
+          'property should be set when manually triggering builds'
       )
 
 
@@ -128,27 +190,28 @@ def GenTests(api):
   for experimental in (True, False):
     for should_upload in (True, False):
       for platform in ('mac', 'linux', 'win'):
-        for branch in ('master', 'beta', 'stable'):
-            for bucket in ('prod', 'staging'):
-              git_ref = 'refs/heads/' + branch
-              test = api.test(
-                  '%s_%s%s%s_%s' % (
-                      platform,
-                      branch,
-                      '_experimental' if experimental else '',
-                      '_upload' if should_upload else '',
-                      bucket
-                  ), api.platform(platform, 64),
-                  api.buildbucket.ci_build(
-                      git_ref=git_ref,
-                      revision=None,
-                      bucket=bucket
-                  ), api.properties(
-                      shard='tests',
-                      fuchsia_ctl_version='version:0.0.2',
-                      upload_packages=should_upload,
-                      gold_tryjob=not should_upload
-                  ), api.runtime(is_experimental=experimental),
-                  api.repo_util.flutter_environment_data()
-              )
-              yield test
+        for branch in ('master', 'beta', 'stable', 'flutter-release-test'):
+            for bucket in ('prod', 'staging', 'flutter'):
+              for git_ref in ('refs/heads/' + branch,
+                'invalid' + branch):
+                  test = api.test(
+                      '%s_%s%s%s_%s' % (
+                          platform,
+                          git_ref,
+                          '_experimental' if experimental else '',
+                          '_upload' if should_upload else '',
+                          bucket
+                      ), api.platform(platform, 64),
+                      api.buildbucket.ci_build(
+                          git_ref=git_ref,
+                          revision=None,
+                          bucket=bucket
+                      ), api.properties(
+                          shard='tests',
+                          fuchsia_ctl_version='version:0.0.2',
+                          upload_packages=should_upload,
+                          gold_tryjob=not should_upload,
+                      ), api.runtime(is_experimental=experimental),
+                      api.repo_util.flutter_environment_data()
+                  )
+                  yield test

05797a5cab15509bb38c360286f68fd7353ab7d7
Jesse Seales <jseales@google.com>
2023-03-15 22:05:33 +0000

    fix ambiguous error
    Change-Id: I1ced6d7286c3bd8514f21433cf0aace8cbb0ef12
    Reviewed-on: https://flutter-review.googlesource.com/c/recipes/+/40521
    Reviewed-by: Godofredo Contreras <godofredoc@google.com>
    Commit-Queue: Jesse Seales <jseales@google.com>

--- recipes/release/release_publish.resources/push_release.sh
+++ recipes/release/release_publish.resources/push_release.sh
@@ -3,11 +3,11 @@
 # Helper script to run auth and git authenticated commands from the same
 # terminal context.
 
-set -e
+set -ex
 gh --version
 gh auth login --hostname github.com --git-protocol https --with-token < $TOKEN_PATH
 gh auth setup-git
-git tag $TAG $RELEASE_GIT__HASH || true
-git rev-list -n 1 $GIT_BRANCH
+git tag $TAG $RELEASE_GIT_HASH || true
+git rev-list -n 1 origin/$GIT_BRANCH
 $DRY_RUN_CMD git push origin $TAG || true
 $DRY_RUN_CMD git push origin HEAD:$RELEASE_CHANNEL

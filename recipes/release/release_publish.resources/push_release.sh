0015f0466c2810355d6d8c5b6dd6b1bfe5bd1f74
Casey Hillers <chillers@google.com>
2023-04-19 22:15:38 +0000

    [release] Dedupe in push release script
    * Add logs to indicate what step is being run to
      make it easy to debug
    
    Bug: None
    Change-Id: I00ffca78d12ca46d55440efad2eb5b864a2f9aae
    Reviewed-on: https://flutter-review.googlesource.com/c/recipes/+/42180
    Commit-Queue: Casey Hillers <chillers@google.com>
    Reviewed-by: Jesse Seales <jseales@google.com>

--- recipes/release/release_publish.resources/push_release.sh
+++ recipes/release/release_publish.resources/push_release.sh
@@ -8,16 +8,9 @@ set -e
 TOKEN=$(cat $TOKEN_PATH)
 git checkout $GIT_BRANCH
 git branch
-if [ $REPO == 'flutter' ]
-then
-  echo 'using tag' $REL_HASH
-  git tag $TAG $REL_HASH || true
-elif [ $REPO == 'engine' ]
-then
-  # for engine, change hash to use output of engine version in flutter/flutter
-  echo 'using tag' $REL_HASH
-  git tag $TAG $REL_HASH || true
-fi
+echo "##### TAGGING #####"
+git tag $TAG $REL_HASH || true
 git remote set-url origin https://$GITHUB_USER:$TOKEN@github.com/flutter/$REPO.git
 git push origin $TAG || true
+echo "##### PUSHING TO $RELEASE_CHANNEL #####"
 git push origin HEAD:$RELEASE_CHANNEL $FORCE_FLAG

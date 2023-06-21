7fd12eecb1fe4cd91d57f617dfc2887899de5d05
Godofredo Contreras <godofredoc@google.com>
2023-06-21 20:16:40 +0000

    Add docs to firebaselab recipe.
    Bug: https://github.com/flutter/flutter/issues/129190
    Change-Id: I7e63d1f927d0992c7a75f97b3ac7a30d8c7e0fcf
    Reviewed-on: https://flutter-review.googlesource.com/c/recipes/+/46341
    Reviewed-by: Reid Baker <reidbaker@google.com>
    Commit-Queue: Godofredo Contreras <godofredoc@google.com>

--- recipes/firebaselab/firebaselab.py
+++ recipes/firebaselab/firebaselab.py
@@ -3,8 +3,9 @@
 # found in the LICENSE file.
 #
 # Recipe to run firebase lab tests.
-# This recipe uses the standar flutter dependencies model and a single property
-# task_name to identify the test to run.
+# This recipe uses the standard flutter dependencies model, a property
+# task_name to identify the test to run and two more properties
+# physical_devices and virtual_devices to specify the devices to use.
 
 from contextlib import contextmanager
 import re
@@ -25,17 +26,22 @@ DEPS = [
 
 
 def RunSteps(api):
-  checkout_path = api.path['start_dir'].join('flutter')
+  # Bucket to upload apks and logs.
   gcs_bucket = 'flutter_firebase_testlab_staging'
+  # Checkout flutter/flutter.
+  checkout_path = api.path['start_dir'].join('flutter')
   api.repo_util.checkout(
       'flutter',
       checkout_path=checkout_path,
       url=api.properties.get('git_url'),
       ref=api.properties.get('git_ref')
   )
+  # Install dependencies. E.g. android sdk.
   env, env_prefixes = api.repo_util.flutter_environment(checkout_path)
   deps = api.properties.get('dependencies', [])
   api.flutter_deps.required_deps(env, env_prefixes, deps)
+
+  # Get the name of the integration test to run.
   task_name = api.properties.get('task_name')
 
   # Initialize to empty lists for use cases when physical_devices or virtual_device
@@ -58,6 +64,10 @@ def RunSteps(api):
           ], 'build/app/outputs/bundle/release/app-release.aab',
           list(physical_devices)
       ),
+      # Use apk because if you let the virtual device pick, it may pick an ARM binary and use
+      # runtime translation. We have seen runtime crashes in ARM translation that don't seem to
+      # occur on physical devices with the same binary. In summary we are trying to make sure
+      # the virtual device is getting a specific architecture that is less likely to crash/have issues.
       (
           'Build apk', [
               'flutter', 'build', 'apk', '--debug', '--target-platform',
@@ -68,6 +78,7 @@ def RunSteps(api):
   )
 
   with api.context(env=env, env_prefixes=env_prefixes, cwd=checkout_path):
+    # Run flutter doctor and update packages.
     api.step('flutter doctor', ['flutter', 'doctor', '-v'])
     api.step(
         'download dependencies',
@@ -78,7 +89,9 @@ def RunSteps(api):
   test_path = checkout_path.join('dev', 'integration_tests', task_name)
   with api.step.nest('test_execution') as presentation:
     with api.context(env=env, env_prefixes=env_prefixes, cwd=test_path):
+      # Collect the task id which is used to generate the logs destination path.
       task_id = api.swarming.task_id
+      # Set the GCP project to use.
       api.gcloud(
           '--quiet',
           'config',
@@ -91,7 +104,9 @@ def RunSteps(api):
         # Skip running gcloud command if no devices were provided.
         if not devices:
           continue
+        # Build the app bundle or apk.
         api.step(step_name, build_command)
+        # Run run the binary in firebaselab using the provided device configurations.
         firebase_cmd = [
             'firebase', 'test', 'android', 'run', '--type', 'robo', '--app',
             binary, '--timeout', '2m',
@@ -121,11 +136,15 @@ def RunSteps(api):
           else:
             raise
 
+      # Download the test logcat files.
       logcat_path = '%s/%s/*/logcat' % (task_name, task_id)
       tmp_logcat = api.path['cleanup'].join('logcat')
       api.gsutil.download(gcs_bucket, logcat_path, api.path['cleanup'])
+      # Read the logcat files and add them to the step logs.
       content = api.file.read_text('read', tmp_logcat)
       presentation.logs['logcat'] = content
+      # Grep logcat files in search of E/flutter log
+      # entries, if found then then fail the test.
       api.step('analyze_logcat', ['grep', 'E/flutter', tmp_logcat], ok_ret=(1,))
 
 

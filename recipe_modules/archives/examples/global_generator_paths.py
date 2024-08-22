cae6cd6e8e2404e61226001427d3ff828b06be33
Alexander Thomas <athom@google.com>
2024-08-22 17:29:15 +0000

    Manual roll (non-trivial) of new path implementation
    Changelog:
    dart:
      f8e5770 Manual non-trivial roll (athom@google.com)
    
    recipe_engine:
      https://crrev.com/d93bcdd661d27c0f32570c24c287781e2ed9bbbe [config_types.Path] Refactor Path implementation. (iannucci@chromium.org)
    
    fuchsia:
      0470115 Roll recipe dependencies (nontrivial).
    
    Change-Id: I370c5a974d536eadcd79b82d5e50a04c77852481
    Reviewed-on: https://flutter-review.googlesource.com/c/recipes/+/59380
    Reviewed-by: Christopher Fujino <fujino@google.com>

--- recipe_modules/archives/examples/global_generator_paths.py
+++ recipe_modules/archives/examples/global_generator_paths.py
@@ -18,7 +18,10 @@ DEPS = [
 
 
 def RunSteps(api):
-  checkout = api.path['start_dir'].join('src')
+  checkout = api.path['start_dir']
+  if api.monorepo.is_monorepo:
+    checkout = checkout / 'monorepo'
+  checkout = checkout / 'src'
   archives = [{
       "source": "out/debug/artifacts.zip",
       "destination": "ios/artifacts.zip",
@@ -71,38 +74,38 @@ def RunSteps(api):
   ]
   expected_monorepo_results = [
       ArchivePaths(
-          local=str(api.path['start_dir'].join('src/out/debug/artifacts.zip')),
+          local=str(api.path['start_dir'].join('monorepo/src/out/debug/artifacts.zip')),
           remote='gs://flutter_archives_v2/monorepo/123/flutter_infra_release/flutter/12345abcde12345abcde12345abcde12345abcde/ios/artifacts.zip'
       ),
       ArchivePaths(
           local=str(
               api.path['start_dir']
-              .join('src/out/release-nobitcode/Flutter.dSYM.zip')
+              .join('monorepo/src/out/release-nobitcode/Flutter.dSYM.zip')
           ),
           remote='gs://flutter_archives_v2/monorepo/123/flutter_infra_release/flutter/12345abcde12345abcde12345abcde12345abcde/ios-release-nobitcode/Flutter.dSYM.zip'
       ),
       ArchivePaths(
           local=str(
-              api.path['start_dir'].join('src/out/release/Flutter.dSYM.zip')
+              api.path['start_dir'].join('monorepo/src/out/release/Flutter.dSYM.zip')
           ),
           remote='gs://flutter_archives_v2/monorepo/123/flutter_infra_release/flutter/12345abcde12345abcde12345abcde12345abcde/ios-release/Flutter.dSYM.zip'
       )
   ]
   expected_monorepo_try_results = [
       ArchivePaths(
-          local=str(api.path['start_dir'].join('src/out/debug/artifacts.zip')),
+          local=str(api.path['start_dir'].join('monorepo/src/out/debug/artifacts.zip')),
           remote='gs://flutter_archives_v2/monorepo_try/123/flutter_infra_release/flutter/123/ios/artifacts.zip'
       ),
       ArchivePaths(
           local=str(
               api.path['start_dir']
-              .join('src/out/release-nobitcode/Flutter.dSYM.zip')
+              .join('monorepo/src/out/release-nobitcode/Flutter.dSYM.zip')
           ),
           remote='gs://flutter_archives_v2/monorepo_try/123/flutter_infra_release/flutter/123/ios-release-nobitcode/Flutter.dSYM.zip'
       ),
       ArchivePaths(
           local=str(
-              api.path['start_dir'].join('src/out/release/Flutter.dSYM.zip')
+              api.path['start_dir'].join('monorepo/src/out/release/Flutter.dSYM.zip')
           ),
           remote='gs://flutter_archives_v2/monorepo_try/123/flutter_infra_release/flutter/123/ios-release/Flutter.dSYM.zip'
       )

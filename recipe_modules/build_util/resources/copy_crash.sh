663697664d556d1e8725ac6ab2d53dd972b5145d
Matan Lurey <matanl@google.com>
2024-08-30 18:26:28 +0000

    Revert "Be more permissive in copying crash files, and log the output."
    This reverts commit fbefb299ef3c975fd8b2f4ce2100ac2834ff7e35.
    
    Reason for revert: Doesn't totally work and/or causes the appearance of failed tasks when it should not (`cp: /Users/chrome-bot/Library/Logs/DiagnosticReports//*.crash: No such file or directory`, https://ci.chromium.org/ui/p/flutter/builders/prod/Mac%20Production%20Engine%20Drone/442318/overview)
    
    Original change's description:
    > Be more permissive in copying crash files, and log the output.
    >
    > Bug: https://github.com/flutter/flutter/issues/154308
    > Change-Id: I9207629fc75698c10fdf3fa96b48df1278a95c30
    > Reviewed-on: https://flutter-review.googlesource.com/c/recipes/+/59560
    > Reviewed-by: Zach Anderson <zra@google.com>
    > Reviewed-by: Christopher Fujino <fujino@google.com>
    > Commit-Queue: Matan Lurey <matanl@google.com>
    
    TBR=zra@google.com,flutter-scoped@luci-project-accounts.iam.gserviceaccount.com,fujino@google.com,matanl@google.com
    
    Change-Id: I9430b31ef50fc4e4b81fb69f33d2ce4bf86d59b7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: https://github.com/flutter/flutter/issues/154308
    Reviewed-on: https://flutter-review.googlesource.com/c/recipes/+/59580
    Reviewed-by: Zach Anderson <zra@google.com>
    Commit-Queue: Matan Lurey <matanl@google.com>
    Reviewed-by: Matan Lurey <matanl@google.com>
    Reviewed-by: Christopher Fujino <fujino@google.com>

--- recipe_modules/build_util/resources/copy_crash.sh
+++ recipe_modules/build_util/resources/copy_crash.sh
@@ -7,17 +7,8 @@
 # bot's default location.
 set -e
 
-# The first argument is the destination directory to copy any crash files to.
 destination=$1
-
-# Crash files are stored in this directory.
-# See https://github.com/flutter/flutter/issues/154308#issuecomment-2318512303.
 origin="$HOME/Library/Logs/DiagnosticReports/"
-
-crash_files=( "$origin"/*.crash )
-if [ ${#crash_files[@]} -gt 0 ]; then
-  cp "${crash_files[@]}" "$destination"/.
-  echo "Crash files copied successfully."
-else
-  echo "No crash files found."
+if [ -f "$origin"/llvm_*.crash ]; then
+  cp "$origin"/llvm_*.crash "$destination"/.
 fi

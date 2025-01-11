4675c1ac2045a570b4bbfa651ab08457870bca2f
Alexander Aprelev <aam@google.com>
2025-01-10 17:54:16 -0800

    Fix flutter web builder by using correct flutter engine checkout path.
    api.repo_util.monorepo_checkout() checks out the flutter engine sources into flutter/engine of its parameter. So if parameter is 'builder/flutter', then  engine ends up in 'builder/flutter/flutter/engine', which breaks bundle_web_sdk_artifacts steps that attempts to find 'builder/flutter/engine/src/flutter/build/zip.py'.
    
    Example of broken build https://ci.chromium.org/ui/p/dart/builders/ci.sandbox/flutter-web/7360/overview
    
    Change-Id: I48cca40309e419d1125499da51ee93ff47c13f8b
    Reviewed-on: https://flutter-review.googlesource.com/c/recipes/+/62000
    Reviewed-by: Alexander Thomas <athom@google.com>
    Commit-Queue: Alexander Aprelev <aam@google.com>

--- recipes/engine_v2/engine_v2.py
+++ recipes/engine_v2/engine_v2.py
@@ -122,7 +122,6 @@ def RunSteps(api):
     # Generators, archives and codesign require a full engine checkout.
     full_engine_checkout = api.path.cache_dir / 'builder'
     if api.monorepo.is_monorepo_ci_build or api.monorepo.is_monorepo_try_build:
-      full_engine_checkout = full_engine_checkout / 'flutter'
       api.file.ensure_directory(
           'Ensure monorepo full engine checkout folder', full_engine_checkout
       )
@@ -131,7 +130,7 @@ def RunSteps(api):
           full_engine_checkout
       )
       api.repo_util.monorepo_checkout(full_engine_checkout, env, env_prefixes)
-      full_engine_checkout = full_engine_checkout / 'engine'
+      full_engine_checkout = full_engine_checkout / 'flutter' / 'engine'
     else:
       api.file.ensure_directory(
           'Ensure full engine checkout folder', full_engine_checkout
